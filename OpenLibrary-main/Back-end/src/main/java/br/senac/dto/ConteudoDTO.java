a5641b386dc7af9af604888cf051e72758a85320
Pablo Henrique <tife.senac@gmail.com>
2024-09-12 21:39:52 -0300

    Add files via upload

---
+++ OpenLibrary-main/Back-end/src/main/java/br/senac/dto/ConteudoDTO.java
@@ -0,0 +1,69 @@
+package br.senac.dto;
+
+import java.sql.Date;
+
+public class ConteudoDTO {
+
+
+
+    private int id;
+    private String usuario;
+    private String Imagem;
+    private String titulo;
+    private String lancamento;
+    private String descricao;
+
+    public ConteudoDTO(String titulo, String lancamento, String descricao){
+        this.titulo = titulo;
+        this.lancamento = lancamento;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getLancamento() {
+        return lancamento;
+    }
+
+    public void setLancamento(String lancamento) {
+        this.lancamento = lancamento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getImagem() {
+        return Imagem;
+    }
+
+    public void setImagem(String imagem) {
+        Imagem = imagem;
+    }
+}
---
+++ OpenLibrary-main/README.md
@@ -0,0 +1,22 @@
+# Open Library Project
+
+>Ferramentas utilizadas na criação do Banco de dados: <br>
+>      <img src="https://hermes.dio.me/articles/cover/cea2ba01-236f-4194-814c-e8651bf1be44.png" width="140px" height="80px" /> <img src="https://miro.medium.com/v2/resize:fit:512/0*ioDeujW3euLCfXew.png" width="150px" height="80px" />
+>
+>Linguagens utilizadas na criação do Banco de dados: <br>
+       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" width="110px" width="100px" height="90px" />
+<br>
+
+>Ferramentas utilizadas na criação do Back-end: <br>
+>      <img src="https://seeklogo.com/images/J/jetbrains-intellij-idea-logo-CA1D5DC51F-seeklogo.com.png" width="80px" height="80px" />
+>
+>Linguagens utilizadas na criação do Back-end: <br>
+       <img src="https://ensinado.com.br/wp-content/uploads/2021/06/java_logo_640.jpg" width="110px" width="100px" height="90px" />
+<br>
+
+>Ferramentas utilizadas na criação do Front-end: <br>
+>      <img src="https://seeklogo.com/images/J/jetbrains-intellij-idea-logo-CA1D5DC51F-seeklogo.com.png" width="80px" height="80px" />
+>
+>Linguagens utilizadas na criação do Front-end: <br>
+       <img src="https://ensinado.com.br/wp-content/uploads/2021/06/java_logo_640.jpg" width="110px" width="100px" height="90px" />
+<br>
