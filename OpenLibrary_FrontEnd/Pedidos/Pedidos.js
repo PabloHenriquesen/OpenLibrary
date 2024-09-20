162642c8ff97df94a8366bd5984921c573efa4c3
Pablo Henrique <tife.senac@gmail.com>
2024-09-19 21:23:27 -0300

    Add files via upload

---
+++ OpenLibrary_FrontEnd/Pedidos/Pedidos.js
@@ -0,0 +1,36 @@
+function gravarConteudo(evento) {
+    evento.preventDefault();
+
+    let usuario = document.getElementById('usuario');
+    let titulo = document.getElementById('titulo');
+    let anoDeLancamento = document.getElementById('anoDeLancamento');
+    let descricao = document.getElementById('descricao');
+    let tipo = document.getElementById('tipo');
+    let imagem = document.getElementById('imagem');
+
+
+    let conteudoJson = {
+        nome: usuario.value,
+        titulo: titulo.value,
+        lancamento: anoDeLancamento.value,
+        descricao: descricao.value,
+        tipo: tipo.value,
+        imagem: imagem.value
+    };
+
+    fetch('http://localhost:8080/conteudo', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(conteudoJson)
+    })
+    .then(function(response){
+        if(response.status === 201){
+            alert('Conteúdo gravado com sucesso!');
+            window.location.href = '/Home/Home.html'
+        }else {
+            alert('Ocorreu um erro ao gravar o conteúdo!');
+        }
+    });
+}
